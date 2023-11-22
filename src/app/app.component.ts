498fce1339b3aa9ad667b5234d1b422c7668fa84
kaustubhchaudhari05 <kaustubh.d.chaudhari38@gmail.com>
2023-11-22 10:58:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserserviceService } from './MyService/userservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'busmanagementangular';
+  newdata:any;
+  showUser: boolean = false;
+  showLogin: boolean = true;
+
+  constructor(private userservice: UserserviceService, private router: Router){}
+
+  openUser(){
+    this.router.navigate(['/user'])
+  }
+
+  openLogin(){
+    this.showLogin = false
+    this.router.navigate(['/login'])
+  }
+ 
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './MyComponent/user/user.component';
+import { LoginPageComponent } from './MyComponents/login-page/login-page.component';
+import { SignupPageComponent } from './MyComponents/signup-page/signup-page.component';
+import { BookingPageComponent } from './MyComponents/booking-page/booking-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable, ReplaySubject} from 'rxjs';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import { AdminBookingComponent } from './MyComponents/admin-booking/admin-booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    BookingPageComponent,
+    AdminBookingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
